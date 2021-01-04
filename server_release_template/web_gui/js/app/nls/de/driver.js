448d4a8313868f912bdf89aef5825b9620d9b50e
Steffen <steffen.zorn@web.de>
2021-01-04 13:02:50 +0100

    Fixed and completed German translation of driver.js

--- server_release_template/web_gui/js/app/nls/de/driver.js
+++ server_release_template/web_gui/js/app/nls/de/driver.js
@@ -2,8 +2,9 @@ define({
     "registeredDrivers": "Registrierte Treiber",
     "noDrivers": "Keine Treiber registriert",
     "removeDriver": "Entfernen",
-    "driverUnregisterSuccessful": "Treiber erfolgreich Entfernt",
+    "driverUnregisterSuccessful": "Treiber erfolgreich entfernt",
     "driverUnregisterFailed": "Treiber konnte nicht entfernt werden",
     "registerAlvrDriver": "ALVR Treiber hinzufügen",
-    "registerAlvrDriverSuccess": "Treiber erfolgreich hinzugefügt"
+    "registerAlvrDriverSuccess": "Treiber erfolgreich hinzugefügt",
+    "driverNotice": "WICHTIG: Seit ALVR v13.1.0 wird die Registrierung des Tribers nicht empfphlen. Die Treiber-Registrierung wird nur noch benötigt, wenn du SteamVR selbst starten möchtest, oder wenn du ALVR zusammen mit anderen SteamVR Treibern wie <a href='https://www.driver4vr.com/'>Driver4VR</a> für Körper-Tracking verwenden möchtest.",
 });
